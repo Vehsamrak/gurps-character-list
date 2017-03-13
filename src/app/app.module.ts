38aa246c3f84063999e46fc736bf0fc364aa227e
Petr Karmashev <smonkl@bk.ru>
2017-03-13 04:48:01 +0300

    HeroService implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule} from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import {AppComponent} from './app.component';
-import {HeroDetailComponent} from "./HeroDetail/hero-detail.component";
+import { AppComponent } from './app.component';
+import { HeroDetailComponent } from "./HeroDetail/hero-detail.component";
 
 @NgModule({
     declarations: [
