cfb973ea96e62df1eab6601740efe888adc8d511
Petr Karmashev <smonkl@bk.ru>
2017-03-15 02:35:49 +0300

    Routing configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from "./Components/HeroDetail/hero-detail.component";
@@ -11,12 +12,18 @@ import { HeroesComponent } from "./Components/Hero/heroes.component";
     declarations: [
         AppComponent,
         HeroDetailComponent,
-        HeroesComponent
+        HeroesComponent,
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        HttpModule
+        HttpModule,
+        RouterModule.forRoot([
+            {
+                path: 'characters',
+                component: HeroesComponent
+            }
+        ]),
     ],
     providers: [],
     bootstrap: [AppComponent]
