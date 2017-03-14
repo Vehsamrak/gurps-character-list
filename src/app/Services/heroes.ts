1bb1ca7537534d3eafbedb1a8792300996ffc6a1
Petr Karmashev <smonkl@bk.ru>
2017-03-14 04:19:57 +0300

    Titles changed

--- src/app/Services/heroes.ts
+++ src/app/Services/heroes.ts
@@ -1,14 +1,9 @@
 import { Hero } from "../Entities/hero";
 
 export const HEROES: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
+    { id: 11, name: 'Warrior' },
+    { id: 13, name: 'Rogue' },
+    { id: 14, name: 'Cultist' },
+    { id: 15, name: 'Wanderer' },
+    { id: 20, name: 'Burglar' }
 ];
