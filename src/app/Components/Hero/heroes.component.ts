c71d16b3052d29584564c5e35d9959e89bb81ed1
Petr Karmashev <smonkl@bk.ru>
2017-03-17 03:48:27 +0300

    CRUD to heroes

--- src/app/Components/Hero/heroes.component.ts
+++ src/app/Components/Hero/heroes.component.ts
@@ -12,8 +12,7 @@ export class HeroesComponent implements OnInit {
     heroes: Hero[];
     selectedHero: Hero;
 
-    constructor(private heroService: HeroService, private router: Router) {
-    }
+    constructor(private heroService: HeroService, private router: Router) {}
 
     ngOnInit(): void {
         this.getHeroes();
@@ -29,5 +28,24 @@ export class HeroesComponent implements OnInit {
 
     gotoDetail(): void {
         this.router.navigate(['/character', this.selectedHero.id]);
+    }
+
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.create(name)
+            .then(hero => {
+                this.heroes.push(hero);
+                this.selectedHero = null;
+            });
+    }
+
+    delete(hero: Hero): void {
+        this.heroService
+            .delete(hero.id)
+            .then(() => {
+                this.heroes = this.heroes.filter(h => h !== hero);
+                if (this.selectedHero === hero) { this.selectedHero = null; }
+            });
     }
 }
